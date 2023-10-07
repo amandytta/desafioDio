3289c50beb1f5e73e5004a46774b3eaee96943dc
amandytta <megami.hina555@gmail.com>
2023-10-07 16:53:47 -0300

    1 update do desafio

--- in.js
+++ in.js
@@ -1,1 +1,23 @@
-hajwje;
+let nome = "andy";
+let exp = 8899;
+let rank
+
+if(exp < 1000) {
+  rank = "ferro"
+} else if (exp > 1001) && (exp < 3000) {
+  rank = "bronze"
+} else if (exp > 3001) && (exp < 5000) {
+  rank = "prata"
+} else if (exp > 5001) && (exp < 7000) { 
+  rank = "ouro"
+} else if (exp > 7001) && (exp < 8000) {
+  rank = "platina"
+} else if (exp > 8001) && (exp < 9000) {
+  rank = "ascendente"
+} else if (exp > 9001) && (exp < 10000) {
+  rank = "imortal"
+} else (exp >= 10001) { 
+  rank = "radiante"
+} 
+
+console.log("O herói de nome " + nome " está no rank de " + rank)
